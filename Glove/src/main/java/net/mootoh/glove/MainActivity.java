76b2589caa12dba5bfb478e94509cd017ecffd98
mootoh <mootoh@gmail.com>
2013-05-29 09:47:03 -0700

    Integrating Parse SDK for push notification.

--- Glove/src/main/java/net/mootoh/glove/MainActivity.java
+++ Glove/src/main/java/net/mootoh/glove/MainActivity.java
@@ -3,7 +3,6 @@ package net.mootoh.glove;
 import android.annotation.TargetApi;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -12,7 +11,10 @@ import android.app.Activity;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
-import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +22,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, "", "");
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
         String message = "normal";
         Intent intent = getIntent();
@@ -40,11 +46,13 @@ public class MainActivity extends Activity {
                 .setContentText("Hello World")
 //                .addAction(R.drawable.ic_launcher, "Paste", null)
                 ;
+
+        // Setting a BroadcastReceiver as a notification action handler.
         Intent resultIntent = new Intent(this, NotifiedReceiver.class);
         resultIntent.putExtra("thrown", "yay");
-
         PendingIntent resultPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(resultPendingIntent);
+
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(1, builder.build());
     }
